de1749ddbd09edb0375927c76ac407c32c67ec2a
dean.lu <dean.lu@ly.com>
2020-09-24 13:59:56 +0800

    fix bug

--- util/system/os.go
+++ util/system/os.go
@@ -13,21 +13,21 @@ import (
 
 type OS struct {
 	// 主机名
-	HostName string `json:"host_name"`
+	HostName string `json:"hostName"`
 	// 真实使用的IP 地址
 	RealIP string `json:"ip"`
 	// MAC地址
 	MAC string `json:"mac"`
 	// IPv4 IP地址
-	IPv4 string `json:"ip_v4"`
+	IPv4 string `json:"ipV4"`
 	// IPv6 ip地址
-	IPv6 string `json:"ip_v6"`
+	IPv6 string `json:"ipV6"`
 	// 系统内核, linux / windows
 	Kernel string `json:"kernel"`
 	// 系统位数 32/64
-	KernelBit int `json:"kernel_bit"`
+	KernelBit int `json:"kernelBit"`
 	// 系统版本,  如: 3.3.0-3.58.24-Rel-2019-05-16_11-57-14-122 / 3.3.0-3.58.22-WIN-Rel-2019-05-15_09-53-18-102
-	KernelVersion string `json:"kernel_version"`
+	KernelVersion string `json:"kernelVersion"`
 	// 平台名，如: CentOS release 6.4 (Final) / Windows Server 2008 R2 Enterprise Service Pack 1 (build 7601)
 	Platform string `json:"platform"`
 }
